f9e441a9dae68d1a68ffb2557f2f219b88c7cda0
21Dev <speedi77@gmail.com>
2025-11-25 21:57:12 -0400

    Initial Commit

---
+++ RFR/RFRApp.swift
@@ -0,0 +1,17 @@
+//
+//  RFRApp.swift
+//  RFR
+//
+//  Created by Anthony Swan on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct RFRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RFRTests/RFRTests.swift
@@ -0,0 +1,16 @@
+//
+//  RFRTests.swift
+//  RFRTests
+//
+//  Created by Anthony Swan on 25/11/2025.
+//
+
+import Testing
+
+struct RFRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
